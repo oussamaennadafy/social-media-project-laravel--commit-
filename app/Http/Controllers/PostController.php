5ebfd0d53274696931cd9c9dc3edc07c7ccbc001
oussama <oussama.ennadafy@gmail.com>
2022-05-26 16:19:48 +0100

    stable version

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
     public function index() {
         $posts = Post::latest()->with(['user', 'likes'])->get();
 
-        return view('posts.index', [ 
+        return view('posts.index' , [ 
             'posts' => $posts
         ]);
     }
